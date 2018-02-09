79f270d4ddebc63cb2b4639fdf2f805229195506
anthony <anthonythi51@gmail.com>
2018-02-09 11:32:44 +0100

    Controller/views

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -7,5 +7,11 @@ use Illuminate\Http\Request;
 class ContactUsController extends Controller
 {
     //
+    public function contactView(){
+      return view('contactUs');
+    }
 
+    public function contactAction(){
+      return redirect()->route('home')->with('success', 'Votre message à bien était envoyer, nous vous répondrons sous peu');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/contact', 'ContactUsController')->name('nous-contacter');
+Route::get('/contact', 'ContactUsController@contactView')->name('nous-contacter-view');
+Route::post('/contact', 'ContactUsController@contactAction')->name('nous-contacter-action');
 
-Route::get('/presentation', 'PresentationController')->name('presentation');
+Route::get('/presentation', 'PresentationController@presentation')->name('presentation');
 
 // Route::get('/newsrss', 'NewRssController')->name('new-rss');
 
-Route::get('/search', 'SearchController')->name('search');
-
-Route::get('/mentions-legales', 'MentionsController')->name('mentions-legales');
+// Route::get('/search', 'SearchController')->name('search-view');
+// Route::post('/search', 'SearchController')->name('search-action');
+//
+// Route::get('/mentions-legales', 'MentionsController')->name('mentions-legales');
