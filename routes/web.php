da1acc97ad98797adc3c54f3a4d1ce12c1a05133
anthony <anthonythi51@gmail.com>
2018-02-09 10:52:10 +0100

    Controllers/views

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/contact', 'ContactUsController')->name('nous-contacter');
+
+Route::get('/presentation', 'PresentationController')->name('presentation');
+
+// Route::get('/newsrss', 'NewRssController')->name('new-rss');
+
+Route::get('/search', 'SearchController')->name('search');
+
+Route::get('/mentions-legales', 'MentionsController')->name('mentions-legales');
