f4bbd60f420799a5ffc5d53b98010ce7ff693fd8
anthony <anthonythi51@gmail.com>
2018-02-13 13:59:28 +0100

    users

--- resources/views/admin/listing-users.blade.php
+++ resources/views/admin/listing-users.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts/appback')
 
 @section('content')
+  <h1>listing des Categories</h1>
+  <a href="{{ route('new-categories') }}">ajouter nouvelle catégorie</a>
 
+  @foreach ($users as $user)
+    {{ $user->name }}
+
+    <a href="{{ route('update-users',['id' => $user->id])}}">modifier</a>
+    {!! Form::open(['route' => ['delete-categories', $user->id], 'method' => 'delete']) !!}
+      {!! Form::submit('Delete') !!}
+    {!! Form::close() !!}
+  @endforeach
 @endsection
--- resources/views/admin/update-users.blade.php
+++ resources/views/admin/update-users.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts/appback')
 
 @section('content')
+  <h1>Formulaire pour modifier un utilisateur</h1>
+  {!! Form::open(['route' => ['update-users-action', $user->id], 'method' => 'put']) !!}
 
+      {{-- Champ Titre --}}
+      {!! Form::label('name', 'Titre de votre categorie') !!}
+
+      {!! Form::text('name', $user->name) !!}
+      {!! $errors->first('name', '<span class="help-block">:message</span>') !!}
+
+      {{-- Champ Titre --}}
+      {!! Form::label('lastname', 'Titre de votre categorie') !!}
+
+      {!! Form::text('lastname', $user->lastname) !!}
+      {!! $errors->first('lastname', '<span class="help-block">:message</span>') !!}
+
+      {{-- Champ Titre --}}
+      {!! Form::label('city', 'Titre de votre categorie') !!}
+
+      {!! Form::text('city', $user->city) !!}
+      {!! $errors->first('city', '<span class="help-block">:message</span>') !!}
+
+      {{-- Champ Titre --}}
+      {!! Form::label('email', 'Titre de votre categorie') !!}
+
+      {!! Form::text('email', $user->email) !!}
+      {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
+
+      {{-- Champ Titre --}}
+      {!! Form::label('password', 'Titre de votre categorie') !!}
+
+      {!! Form::text('password', $user->password) !!}
+      {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
+
+      {{-- Champ Titre --}}
+      {!! Form::label('role', 'Titre de votre categorie') !!}
+
+      {!! Form::select('role', array('actif' => 'Actif', 'inactif' => 'Inactif')) !!}
+
+
+
+      {!! Form::submit('envoyer') !!}
+  {!! Form::close() !!}
 
 @endsection
