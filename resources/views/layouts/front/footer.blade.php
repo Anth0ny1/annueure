da6f630acd1bb9d8666151cc6f29f2c00d4f414b
franck <thekingofpizzas@gmail.com>
2018-02-16 16:16:09 +0100

    franck16/02pushmatin

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -6,20 +6,51 @@
   <div class="footerPresentation"> <!-- footer -->
 
     <div class="footerContainer">
-      <ul>
+      {{-- <ul>
         <li><a href="#">nous contacter</a></li>
         <li><a href="#">3 derniers pro inscrits</a></li>
         <li><a href="#">remerciements</a></li>
-      </ul>
+        <li><a href="#">plan du site</a></li>
+      </ul> --}}
+      <div class="footerContainerBox"><a href="#">nous contacter</a><br /><br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </p></div>
+      <div class="footerContainerBox"><a href="#">3 derniers pro inscrits</a><br /><br />
+      <p>pro1 Loic le plombier spécialiste du débouchage de vieux tuyaux poilus<br /><br />
+       pro2 Anthony <br /><br />
+       pro3 Jerome</p></div>
+      <div class="footerContainerBox"><a href="#">remerciements</a><br /><br />
+      <p>Le site internet Annu'Eure a été réalisé dans le cadre d'une soutenance
+        de formation au codage et développement web sous l'égide de la WebForce 3
+        et financé par la Chambre de Commerce et d'industrie de l'Eure</p></div>
+      <div class="footerContainerBox"><a href="#">plan du site</a><br /><br />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p></div>
+      <div class="footerContainerBox"><a href="#">Mentions légales</a><br /><br />
+      <p></p></div>
     </div>
       <div>
         <img class="mapEure" src="admin/img/mapEure.png" alt="carte qui représente le département de l'eure" />
       </div>
+
   </div> <!-- end footer -->
+      <div class="logosCCIEure">
+        <img src="img/logos/286px-Logo_Région_Normandie.png" alt="" style="height:70px;">
+        <img src="img/logos/bd464ab0c6bcd31872784ab8e404292bc2868788.jpg" alt="" style="height:60px;">
+        <img src="img/logos/logo_opquast.png" alt="" style="height:60px;">
+        <img src="img/logos/logo-eure.png" alt="" style="height:60px;">
+        <img src="img/logos/logo-webforce3.svg" alt="" style="height:60px;">
+      </div>
 
+</footer> <!-- end footer -->
 
-</footer>
-<!-- end footer -->
 
 <!-- start Javascript -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
