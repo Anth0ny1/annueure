63f35477f204f6ae5d918e95c4a09f0ef75e571a
studiojb <brachet.jerome@gmail.com>
2018-02-16 03:43:43 +0100

    JB modif divers pictos 16-02-2018 a 4h00

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -1,6 +1,7 @@
+<!-- start footer -->
 <footer id="footer">
 
-  <div class="footerPresentation"> <!-- footer -->
+  <div class="footerPresentation">
     <ul>
       <li><a href="#">plan du site</a></li>
       <li><a href="#">mentions-légales</a></li>
@@ -8,14 +9,14 @@
       <li><a href="#">partenaires</a></li>
     </ul>
     <div class="mapEure">
-    <img src="admin/img/mapEure.png" alt="carte qui représente le département de l'eure" />
+      <img style="float: right;" src="admin/img/mapEure.png" alt="carte qui représente le département de l'eure" height="350" width="650" />
     </div>
-  </div> <!-- end footer -->
-
-
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  </div>
 </footer>
+<!-- end footer -->
+
+<!-- start Javascript -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- end Javascript -->
