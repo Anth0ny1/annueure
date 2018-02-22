32da66db4d926593945d5349b160beef5bf176bf
anthony <anthonythi51@gmail.com>
2018-02-22 08:49:55 +0100

    matinjeudi

--- resources/views/layouts/front/footer.blade.php
+++ resources/views/layouts/front/footer.blade.php
@@ -1,6 +1,6 @@
 
 
-<footer id="footer">
+{{-- <footer id="footer"> --}}
 
 
   <!-- start footer -->
@@ -59,7 +59,6 @@
 
 
 
-
     {{-- <div class="logosCCIEure">
       <img src="{{ asset('img/logos/logo_region_normandie_286px.png') }}" alt="" style="height:60px;" />
       <img src="{{ asset('img/logos/logo_cci_formation_eure.jpg') }}" alt="" style="height:60px;" />
@@ -73,8 +72,10 @@
 
 
 
-</footer> <!-- end footer -->
+{{-- </footer> <!-- end footer --> --}}
 
+
+</div>
     <div id="footerMap">
       <iframe id="footerIframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d334631.6632497623!2d0.854337880896347!3d49.06174431076737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e13fd4a26e28c7%3A0x30c14484fb52710!2sEure!5e0!3m2!1sfr!2sfr!4v1518787100973" frameborder="0" allowfullscreen></iframe>
     </div>
